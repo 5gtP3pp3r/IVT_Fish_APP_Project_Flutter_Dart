d2dc951ea2ce7a13718ed578f356e92cdcd47177
5gtp3pp3r <william.crepault@gmail.com>
2025-04-30 11:17:03 -0400

    Ajout temp getAccessToken() et début fonction identifyFish

--- fish_app/lib/identity_fish_API.dart
+++ fish_app/lib/identity_fish_API.dart
@@ -8,9 +8,25 @@ Future<int> getImageByteSize(String picturePath) async {
   return await file.length();
 }
 
-Future<String> getMd5ChecksumBase64(String path) async {
-  final file = File(path);
+Future<String> getMd5ChecksumBase64(String picturePath) async {
+  final file = File(picturePath);
   final bytes = await file.readAsBytes();
   final digest = md5.convert(bytes);
   return base64Encode(digest.bytes);
+}
+
+Future<String> getAccessToken() async {
+
+}
+
+
+
+Future<String> identifyFish(String picturePath) async {
+  final String filname = picturePath.split("/").last;
+  final String mime = "image/jpeg";
+  final int byteSize = await getImageByteSize(picturePath);
+  final String checksum = await getMd5ChecksumBase64(picturePath);
+
+
+
 }
