afd2f00a33c002e57c1d454e3d9bb7f26c15a8ba
5gtp3pp3r <william.crepault@gmail.com>
2025-04-30 11:09:17 -0400

    Ajout fonction MD5 checksum

--- fish_app/lib/identity_fish_API.dart
+++ fish_app/lib/identity_fish_API.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:convert';
+import 'package:crypto/crypto.dart';
 import 'package:http/http.dart' as http;
 
 Future<int> getImageByteSize(String picturePath) async {
@@ -7,4 +8,9 @@ Future<int> getImageByteSize(String picturePath) async {
   return await file.length();
 }
 
-
+Future<String> getMd5ChecksumBase64(String path) async {
+  final file = File(path);
+  final bytes = await file.readAsBytes();
+  final digest = md5.convert(bytes);
+  return base64Encode(digest.bytes);
+}
