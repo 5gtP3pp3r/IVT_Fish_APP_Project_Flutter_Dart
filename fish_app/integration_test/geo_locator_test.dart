091360f8611030159c4bc0ac2d4c1d0eb0c949e1
5gtp3pp3r <william.crepault@gmail.com>
2025-05-07 20:15:05 -0400

    modification de test, ajout du rep integration_test pour pouvoir utiliser le cell comme plateforme de test

---
+++ fish_app/integration_test/geo_locator_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:fish_app/api/geo_locator.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Test réel du GeoLocator', (WidgetTester tester) async {
+    final geo = GeoLocator();
+    final result = await geo.getCurrentLocation();
+
+    print('🛰 Résultat de géolocalisation: $result');
+    expect(result, contains('|'));
+  });
+}
